f08455bf574d8dcd936086a9eae20ff5ee5be267
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2024-09-20 21:49:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/numad24fa_chunlingji/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.numad24fa_chunlingji;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button showToastButton = findViewById(R.id.showToastButton);
+        showToastButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            String message = "Name: Chunling Ji\nEmail: ji.chu@northeastern.edu";
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
